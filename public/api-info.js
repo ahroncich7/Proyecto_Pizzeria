cda67c9dd87e400f2614dd1728a5b33cbac4b7fe
ilaoming <jesus.rios2609@gmail.com>
2022-01-23 10:28:08 -0500

    Change api-info.js

--- public/api-info.js
+++ public/api-info.js
@@ -1,10 +1,10 @@
-apiData:{
-[
-    ["Api home page","https://laomingcode-node-mysql.herokuapp.com"],
+const apiData = [
     ["Return all categories","https://laomingcode-node-mysql.herokuapp.com/api/category"],
-    ["Return category by id","https://laomingcode-node-mysql.herokuapp.com/api/category/:id"],
+    ["Return category by id","https://laomingcode-node-mysql.herokuapp.com/api/category/:id","Example: https://laomingcode-node-mysql.herokuapp.com/api/category/1"],
     ["Return all products","https://laomingcode-node-mysql.herokuapp.com/api/product"],
-    ["Return product by id","https://laomingcode-node-mysql.herokuapp.com/api/product/:id"],
-    ["Return product by category","https://laomingcode-node-mysql.herokuapp.com/api/product/category/:category_id"],
+    ["Return product by id","https://laomingcode-node-mysql.herokuapp.com/api/product/:id","Example: https://laomingcode-node-mysql.herokuapp.com/api/product/1"],
+    ["Return product by category","https://laomingcode-node-mysql.herokuapp.com/api/product/category/:category_id","Example: https://laomingcode-node-mysql.herokuapp.com/api/product/category/1"],
+    ["Post category *required 'name' in the body","https://laomingcode-node-mysql.herokuapp.com/api/category/"],
+    ["Post product *required 'name,description,price,foto,available,category_id' in the body","https://laomingcode-node-mysql.herokuapp.com/api/product/"]
+
 ]
-}

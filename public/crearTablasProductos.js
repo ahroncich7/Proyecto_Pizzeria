f78dd7132a7652dc8f2a0aa37d22fc6b462537e3
ahroncich7 <alehroncich@gmail.com>
2022-01-30 15:42:43 -0300

    Agrega manejar errores y devolver una promesa para la funcion crearTablas()

--- public/crearTablasProductos.js
+++ public/crearTablasProductos.js
@@ -5,8 +5,9 @@ import {
 export function crearTablas(id, url) {
     let tabla = document.getElementById(id)
     tabla.innerHTML = `<p>Cargando...</p>`;
-    fetch(url)
+    return fetch(url)
         .then((r) => r.json())
         .then((data) => crearTarjetas(data.data, tabla))
-
+        .then((r) => r)
+        .catch((error) => console.log(error))
 }

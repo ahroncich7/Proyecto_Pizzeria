7a411bb451743590fcdc2f043724d625dc2ec021
ahroncich7 <alehroncich@gmail.com>
2022-01-30 16:43:00 -0300

    exporta funciones desde declaración general

--- public/crearTablasProductos.js
+++ public/crearTablasProductos.js
@@ -2,7 +2,12 @@ import {
     crearTarjetas
 } from "./crearElementos.js";
 
-export function crearTablas(id, url) {
+export {
+    crearTablas,
+    cargarDatosEnModalAgregarAlPedido
+}
+
+function crearTablas(id, url) {
     let tabla = document.getElementById(id)
     tabla.innerHTML = `<p>Cargando...</p>`;
     return fetch(url)
@@ -12,7 +17,7 @@ export function crearTablas(id, url) {
         .catch((error) => console.log(error))
 }
 
-export function cargarDatosEnModalAgregarAlPedido(product) {
+function cargarDatosEnModalAgregarAlPedido(product) {
     document.getElementById("carga-productos-img").src = `./images/cat${product.category_id}id${product.id}.jpg`;
     document.getElementById("carga-productos-subtitle").textContent = `${product.name} ${product.price}`;
     document.getElementById("carga-productos-descripcion").textContent = `${product.description}`;

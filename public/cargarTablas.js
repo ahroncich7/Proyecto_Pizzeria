bdf833f8abbf6b4da500a4e69f9ab8c8c37e6a52
ahroncich7 <alehroncich@gmail.com>
2022-02-09 15:29:47 -0300

    Reemplaza __ por - en id

--- public/cargarTablas.js
+++ public/cargarTablas.js
@@ -25,7 +25,6 @@ function crearTarjetas(products, sectionElement) {
 };
 
 async function cargarTablas(IdHTML, catId) {
-
     let products = await obtenerProductosDeAPI(catId)
     let tabla = document.getElementById(IdHTML)
     crearTarjetas(products, tabla)
@@ -34,7 +33,7 @@ async function cargarTablas(IdHTML, catId) {
 
 
 function cargarDatosEnModalAgregarAlPedido(product) {
-    document.getElementById("add_to_order_window").setAttribute("product-id", `${product.id}`)
+    document.getElementById("add-to-order-window").setAttribute("product-id", `${product.id}`)
     document.getElementById("carga-productos-img").src = `${product.picture}`;
     document.getElementById("carga-productos-subtitle").textContent = `${product.name} $${product.price}`;
     document.getElementById("carga-productos-descripcion").textContent = `${product.description}`;
